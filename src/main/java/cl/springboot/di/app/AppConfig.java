c65314a3d2a420201a7bff019c6222ca14c0114d
Juan Daniel Muñoz Queupul <munoz.queupul.daniel.juan@gmail.com>
2020-09-08 16:33:06 -0300

    AppConfig crear 2 listas en la clase implementaciones de la misma manera que se hace con las anotaciones @qualifier y @primary dentro de una clase

--- src/main/java/cl/springboot/di/app/AppConfig.java
+++ src/main/java/cl/springboot/di/app/AppConfig.java
@@ -1,9 +1,15 @@
 package cl.springboot.di.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+
+import cl.springboot.di.app.models.domain.ItemFactura;
+import cl.springboot.di.app.models.domain.Producto;
 import cl.springboot.di.app.services.IMiServicio;
 import cl.springboot.di.app.services.impl.MiServicioComplejoImpl;
 import cl.springboot.di.app.services.impl.MiServicioImpl;
@@ -20,5 +26,28 @@ public class AppConfig {
 	public IMiServicio registrarMiServicioComplejo() {
 		return new MiServicioComplejoImpl();
 	}
+	
+	@Bean("itemsFactura")
+	public List<ItemFactura> registrarItems(){
+		return Arrays.asList(
+				new ItemFactura(new Producto("Guitarra", 10000), 20),
+				new ItemFactura(new Producto("Tambor", 2000), 35),
+				new ItemFactura(new Producto("pandero", 54780), 5),
+				new ItemFactura(new Producto("microfono", 54780), 5)
+				);
+		
+	}
 
+	@Bean("itemsFacturaOficina")
+	@Primary
+	public List<ItemFactura> registrarItemsOficina(){
+		return Arrays.asList(
+				new ItemFactura(new Producto("Mouse", 10000), 20),
+				new ItemFactura(new Producto("Pc de Escritorio", 530000), 2),
+				new ItemFactura(new Producto("Monitores LCD 24 pulgadas", 54780), 5),
+				new ItemFactura(new Producto("Teclado Tech", 54780), 5)
+				);
+		
+	}
+	
 }
---
+++ src/main/java/cl/springboot/di/app/controllers/FacturaController.java
@@ -0,0 +1,27 @@
+package cl.springboot.di.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cl.springboot.di.app.models.domain.Factura;
+
+@Controller
+@RequestMapping("/factura")
+public class FacturaController {
+	
+	@Autowired
+	Factura factura;
+	
+	@GetMapping("/ver")
+	public String ver(Model model) {
+		
+		model.addAttribute("titulo", "Pagina de Factura");
+		model.addAttribute("factura", factura);
+		
+		return "factura/ver";
+		
+	}
+}
