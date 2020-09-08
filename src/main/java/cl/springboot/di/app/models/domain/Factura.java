bf3d6ade22d1c0452ab694d02ac5748815c64e46
Juan Daniel Muñoz Queupul <munoz.queupul.daniel.juan@gmail.com>
2020-09-08 17:17:58 -0300

    se aprende a utilizar el @postConstruct y @predestroy

--- src/main/java/cl/springboot/di/app/models/domain/Factura.java
+++ src/main/java/cl/springboot/di/app/models/domain/Factura.java
@@ -2,6 +2,9 @@ package cl.springboot.di.app.models.domain;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,6 +22,18 @@ public class Factura {
 	@Autowired
 	private List<ItemFactura> items;
 
+	
+	@PostConstruct
+	public void inicializar() {
+		cliente.setApellido(cliente.getNombre().concat(" ").concat("Juanuko"));
+		descripcion = descripcion.concat(" del cliente :").concat(cliente.getNombre());
+	}
+	
+	@PreDestroy
+	public void destruir() {
+		System.out.println(" Factura destruida:".concat(descripcion));
+	}
+	
 	public String getDescripcion() {
 		return descripcion;
 	}
